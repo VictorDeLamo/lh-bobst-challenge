af26b09122940490ca7c02aa0f08c0686b81f9b7
santiago.oliver <santiago.oliver@estudiantat.upc.edu>
2024-12-01 02:25:09 +0100

    Basic information

--- main.py
+++ main.py
@@ -1,56 +1,114 @@
 import RPi.GPIO as GPIO
-import time
+from ina219 import INA219, DeviceRangeError
+from azure.iot.device import IoTHubDeviceClient, Message
+import time, datetime, json
 
-# PINS
+# VARS
+boxes = 0
+currentPower = 0
+speed = 0
+dutyCycle = 12
+start_time = time.time()
+total_seconds = 0
+mwatts = 1.0
+
+# CONST
 SERVO_PIN = 16
 TRIG_PIN = 17
 ECHO_PIN  = 27
+LINEAL_PIN = 12
+SHUNT_OMHS = 0.1
+CONNECTION_STRING = 'HostName=ra-develop-bobstconnect-01.azure-devices.net;DeviceId=LAUZHACKPI3;SharedAccessKey=wALbROiWbQnS3jd7/PtmQXEBAUqKu0iStAIoTMC95ps='
+MACHINE_ID = 'lauzhack-pi3'
+MACHINE_IP = '10.0.4.4:80'
 
 # SETUP
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SERVO_PIN, GPIO.OUT)
 GPIO.setup(TRIG_PIN, GPIO.OUT)
 GPIO.setup(ECHO_PIN, GPIO.IN)
+GPIO.setup(LINEAL_PIN, GPIO.IN)
 
-# Define PWM for ServoMotor
+ina = INA219(shunt_ohms=SHUNT_OMHS, busnum=1)
+ina.configure(
+    voltage_range=ina.RANGE_16V,
+    gain=ina.GAIN_AUTO,
+    bus_adc=ina.ADC_128SAMP,
+    shunt_adc=ina.ADC_128SAMP
+)
+
 pwm = GPIO.PWM(SERVO_PIN, 50)
 pwm.start(0)
 
-def measure_distance():
-    # 10µs pulse
-    GPIO.output(TRIG_PIN, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG_PIN, False)
-    
-    # Wait for ECHO pulse
-    while GPIO.input(ECHO_PIN) == 0:
-        pulse_start = time.time()
-    
-    while GPIO.input(ECHO_PIN) == 1:
-        pulse_end = time.time()
-    
-    # Calculate pulse duration
-    lapse = pulse_end - pulse_start
-    # Calculate distance in cm
-    distance = lapse * 34300 / 2  # Sound speed: 343 m/s
-    return distance
+client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
 
-try:
-    print("Servo is running continuously. Press Ctrl+C to stop.")
-    while True:
-        # Update servo
-        pwm.ChangeDutyCycle(10)
-        time.sleep(0.001)
+def servo_motor():
+    global pwm
+    pwm.ChangeDutyCycle(dutyCycle)
+    time.sleep(0.001)
 
-        # Measure distance
-        distance = measure_distance()
-        print(f"Distance: {distance:.2f} cm")
+def power():
+    global mwatts
+    mwatts += ina.power()
+    time.sleep(1)
+
+def update_speed():
+    global current_time, start_time, speed, total_seconds
+    current_time = time.time()
+    if current_time - start_time >= 1:
+        total_seconds +=1
+        speed = boxes/total_seconds
+        start_time = current_time
+
+def update_boxes():
+    global sensor_bool, boxes
+    sensor_bool = GPIO.input(LINEAL_PIN)
+    if sensor_bool == 0:
+        boxes = boxes + 1
+
+def send_telemetry():
+        global boxes, speed, mwatts
+        telemetry_data = {
+            "telemetry": {
+                "timestamp": datetime.datetime.utcnow().isoformat() + "Z",
+                "datasource": MACHINE_IP,
+                "machineid": MACHINE_ID,
+                "totaloutputunitcount": boxes,
+                "machineSpeed": speed,
+                "totalworkingenergy": mwatts
+            }
+        }
+        telemetry_json = json.dumps(telemetry_data)
+        message = Message(telemetry_json)
+        message.content_type = "application/json"
+        message.content_encoding = "utf-8"
+        message.custom_properties["messageType"] = "Telemetry"
+
+        try:
+            client.send_message(message)
+            print(f"Telemetry sent: {telemetry_json}")
+        except Exception as e:
+            print(f"Failed to send telemetry: {e}")
+
+def main(): 
+    try:
+        print("Servo is running continuously. Press Ctrl+C to stop.")
+        servo_motor()
+        while True:
+            power()
+            update_boxes()
+            update_speed()
+            send_telemetry()
+
+    except KeyboardInterrupt:
+        print("\nStopping the servo.")
+        pwm.ChangeDutyCycle(0)
         time.sleep(1)
 
-except KeyboardInterrupt:
-    print("\nStopping the servo.")
-    pwm.ChangeDutyCycle(0)
-    time.sleep(1)
-finally:
-    pwm.stop()
-    GPIO.cleanup()
+    finally:
+        pwm.stop()
+        GPIO.cleanup()
+        client.shutdown()
+
+if __name__ == "__main__":
+        main()
